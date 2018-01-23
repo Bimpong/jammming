11e4f126b089c1b4fdf7d9bdf594bd35db8afb70
Bimpong <amoakooseiyaw@gmail.com>
2018-01-23 16:51:37 +0000

    Add files via upload

---
+++ src/Components/App/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+import {Playlist} from '../Playlist/Playlist';
+import TrackList from '../TrackList/TrackList';
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: {
+        name: 'Pop Heads',
+        artist: 'Pu5i_Eater',
+        album: 'R6 Tunes'
+      }
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResult={this.state.searchResults}/>
+            <Playlist />
+            <TrackList />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+
+export class TrackList extends React.Component{
+  render(){
+    return(
+      <div className="TrackList"> 
+        
+        {this.props.tracks.map(function(track){
+          return <Track key={track.id} />
+        })}
+        
+      </div>
+          )
+        }
+}
+
+export default TrackList;
